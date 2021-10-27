1d56c39446f452a4d03ca4a372249be6617d5758
asuravadha <rr.easwar@gmail.com>
2021-10-27 08:56:16 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+    float a=13,b=2;
+    System.out.println("a is "+a+"\nb is "+b);
+    System.out.println("a+b is "+(a+b));
+    System.out.println("a-b is "+(a-b));
+    System.out.println("a*b is "+(a*b));
+    System.out.println("a/b is "+(a/b));
+    System.out.println("a%b is "+(a%b));
+
+    }
+}
