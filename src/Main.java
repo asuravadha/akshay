a3b077f03c6778bd7a1c6c74247f25886fabf5c3
asuravadha <rr.easwar@gmail.com>
2021-10-28 08:46:24 +0530

    control statements

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,71 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        int age;
+        int a,b;
+        String operation;
+        System.out.println("enter a,b values");
         Scanner input=new Scanner(System.in);
-        System.out.print("enter  your age :");
-        age=input.nextInt();
-        if(age>18){
-            System.out.print("\neligible because your age is "+age);
+        a=input.nextInt();
+        b=input.nextInt();
+        System.out.println("choose any one of this (+,-,*,/,%) to perform operation");
+        operation=input.next();
+        if(operation.equals("+")){
+            System.out.println("a+b is "+(a+b));
+        }
+         else if(operation.equals("-")){
+            System.out.println("a-b is "+(a-b));
+        }
+        else if(operation.equals("*")){
+            System.out.println("a*b is "+(a*b));
+        }
+        else if(operation.equals("/")){
+            System.out.println("a/b is "+(a/b));
+        }
+        else if(operation.equals("%")){
+            System.out.println("a%b is "+(a%b));
         }
         else{
-            System.out.print("\nnot eligible because your age is "+age);
+            System.out.println("invalid operation");
         }
+
+
+
+
+
+
+
+//        int a;
+//        Scanner input=new Scanner(System.in);
+//        a=input.nextInt();
+//        if(a>0){
+//            System.out.println("positive");
+//            if(a<=100){
+//                System.out.println("its below 100");
+//            }
+//            else{
+//                System.out.println("its above 100");
+//            }
+//        }
+//        else if(a<0){
+//            System.out.println("negative");
+//        }
+//        else{
+//            System.out.println("zero");
+//        }
+
+
+
+
+//        int age;
+//        Scanner input=new Scanner(System.in);
+//        System.out.print("enter  your age :");
+//        age=input.nextInt();
+//        if(age>18){
+//            System.out.print("\neligible because your age is "+age);
+//        }
+//        else{
+//            System.out.print("\nnot eligible because your age is "+age);
+//        }
 
 
 
